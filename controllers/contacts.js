38e0ee7d9f021f7c54cacc29cb0a3a8453a18299
nzend <115723110+nzend@users.noreply.github.com>
2023-07-30 21:31:38 +0300

    Fix getAll

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -21,13 +21,6 @@ const getAll = async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
 
-  if (Object.keys(req.query) < 1) {
-    const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-      skip,
-      limit,
-    }).populate("owner", "email");
-    res.status(200).json(result);
-  }
   const result = await Contact.find(
     { owner, ...req.query },
     "-createdAt -updatedAt",
