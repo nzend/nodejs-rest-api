9424334afc4096e1347df71b745e781a526dbcc9
nzend <115723110+nzend@users.noreply.github.com>
2023-07-31 12:52:09 +0300

    add jimp resize

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,7 @@ const Joi = require("joi");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 // const emailRegexp = new RegExp("/^[^s@]+@[^s@]+.[^s@]+$/");
 
@@ -93,16 +94,23 @@ const updateSubscription = async (req, res) => {
   res.status(200).json({ subscription });
 };
 const updateAvatar = async (req, res) => {
-  const { _id, } = req.user;
-  console.log(req.user.avatarURl);
+  const { _id } = req.user;
+
   const { path: tempUpload, originalname } = req.file;
+
+  Jimp.read(tempUpload, (error, image) => {
+    if (error) throw HttpError(404, "Not found");
+    console.log(avatarsDir);
+    image.resize(250, 250).write(`${avatarsDir}/${_id}_${originalname}`);
+  });
+
   const fileName = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, fileName);
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", fileName);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({ avatarURL });
-}; 
+};
 
 module.exports = {
   register: ctrlWrapper(register),
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,7 +1,9 @@
 const multer = require("multer");
 const path = require("path");
 
+
 const tempDir = path.join(__dirname, "../", "temp");
+
 
 const multerConfig = multer.diskStorage({
   destination: tempDir,
